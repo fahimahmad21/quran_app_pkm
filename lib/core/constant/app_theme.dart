946a9aec6fb6c8b3fd9851b246028ce5a356b42d
muhmmdfath <fatihadli709@gmail.com>
2024-11-12 21:54:48 +0700

    apptheme

---
+++ lib/core/constant/app_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app_pkm/core/constant/app_colors.dart';
+
+class AppTheme {
+  static ThemeData get light => ThemeData(
+    primaryColor: AppColors.primaryColor,
+    scaffoldBackgroundColor: AppColors.backgroundColor,
+    textTheme: const TextTheme(
+      headlineLarge: TextStyle(
+        fontSize: 34,
+        fontWeight: FontWeight.w500,
+        color: AppColors.dashboardColor,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 16,
+        color: AppColors.backgroundColor,
+      )
+    )
+  );
+}
